a71e3c555e2d563be3b6de62494f9cf9c34c3faa
kavinkumar06 <kavinm09@gmail.com>
2017-07-14 15:52:22 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,17 @@
+# ASCII
+import java.io.*;
+import java.util.*;
+public class ASCII
+{
+  public static void main(String args[])
+  {
+    String ch;
+        int i;
+        for(i=1; i<=255; i++)
+        {
+            ch = new Character((char)i).toString();
+            System.out.print(i+ " -> " + ch + "\t");
+        }
+		
+    }
+}
